864615a48451d37e15b5b35791c25c471b5b8aa9
PatriotRossii <patriotrossii2019@mail.ru>
2021-04-15 19:45:23 +0500

    Upload writing part

---
+++ src/writing.rs
@@ -0,0 +1,65 @@
+extern crate csv;
+extern crate serde;
+ #[macro_use]
+extern crate serde_derive;
+
+use std::{env, error::Error, ffi::OsString};
+use std::process;
+
+#[derive(Debug, Serialize)]
+#[serde(rename_all = "PascalCase")]
+struct Record<'a> {
+   city: &'a str,
+   state: &'a str,
+   population: Option<u64>,
+   latitude: f64,
+   longitude: f64,
+}
+
+
+fn run() -> Result<(), Box<dyn Error>> {
+    let file_path = get_first_arg()?;
+    let mut wtr = csv::WriterBuilder::new()
+        .delimiter(b'\t')
+        .quote_style(csv::QuoteStyle::NonNumeric)
+        .from_path(file_path)?;
+
+    wtr.serialize(Record {
+        city: "Davidsons Landing",
+        state: "AK",
+        population: None,
+        latitude: 65.2419444,
+        longitude: -165.2716667,
+    })?;
+    wtr.serialize(Record {
+        city: "Kenai",
+        state: "AK",
+        population: Some(7610),
+        latitude: 60.5544444,
+        longitude: -151.2583333,
+    })?;
+    wtr.serialize(Record {
+        city: "Oakman",
+        state: "AL",
+        population: None,
+        latitude: 33.7133333,
+        longitude: -87.3886111,
+    })?;
+
+    wtr.flush()?;
+    Ok(())
+}
+
+fn get_first_arg() -> Result<OsString, Box<dyn Error>> {
+    match env::args_os().nth(1) {
+        None => Err(From::from("expected 1 argument, but got none")),
+        Some(file_path) => Ok(file_path),
+    }
+}
+
+fn main() {
+    if let Err(err) = run() {
+        println!("{}", err);
+        process::exit(1);
+    }
+}
